310ecc97c2e0e57513f036986089960b35367af9
Dat Nguyen Thanh <dat.nt31@shbfinance.com.vn>
2025-02-17 20:24:07 +0700

    Update camunda client

--- src/main/java/io/camunda/demo/process_payments/ChargeCreditCardWorker.java
+++ src/main/java/io/camunda/demo/process_payments/ChargeCreditCardWorker.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.camunda.zeebe.spring.client.annotation.JobWorker;
-import io.camunda.zeebe.spring.client.annotation.Variable;
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.EvaluateDecisionResponse;
+import io.camunda.client.impl.oauth.OAuthCredentialsProvider;
+import io.camunda.client.impl.oauth.OAuthCredentialsProviderBuilder;
 
 @Component
 public class ChargeCreditCardWorker {
@@ -16,11 +19,38 @@ public class ChargeCreditCardWorker {
 
   @JobWorker(type = "charge-credit-card")
   public Map<String, Object> chargeCreditCard(@Variable(name = "totalWithTax") Double totalWithTax) {
-    LOG.info("charging credit card: {}", totalWithTax);
-    Map<String, Object> result = new HashMap<>();
-    result.put("amountCharged", totalWithTax);
-    result.put("currentBalance", totalWithTax - 10000);
+  private static final String zeebeGrpc = "[Zeebe Address e.g. f887f1a6-7c2b-48ce-809a-e11e5a6ba31a.dsm-1.zeebe.camunda.io:443]";
+  private static final String zeebeRest = "[Zeebe Address e.g. https://dsm-1.zeebe.camunda.io/f887f1a6-7c2b-48ce-809a-e11e5a6ba31a]";
+  private static final String audience = "[Zeebe Token Audience, e.g., zeebe.camunda.io]";
+  private static final String clientId = "[Client ID, e.g., FmT7K8gVv_FcwiUhc8U-fAJ9wph0Kn~P]";
+  private static final String clientSecret = "[Client Secret]";
+  private static final String oAuthAPI = "[OAuth API, e.g., https://login.cloud.camunda.io/oauth/token] ";
 
-    return result;
+  @Autowired
+  private CamundaClient client;
+
+  public void chargeCreditCard(String[] args) {
+    OAuthCredentialsProvider credentialsProvider = new OAuthCredentialsProviderBuilder()
+        .authorizationServerUrl(oAuthAPI)
+        .audience(audience)
+        .clientId(clientId)
+        .clientSecret(clientSecret)
+        .build();
+
+    System.out.println("Evaluating decision");
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("currency", "VND");
+    variables.put("exchangeRate", 25000);
+    variables.put("currentPaymentAmount", 2000);
+    variables.put("currentPaymentAmountVND", 50000000);
+
+    final EvaluateDecisionResponse decisionEvaluation = client
+        .newEvaluateDecisionCommand()
+        .decisionId("check_payment_amount")
+        .variables("{\"lightsaberColor\": \"blue\"}")
+        .send()
+        .join();
+
+    LOG.info("response info get decision: {}", decisionEvaluation);
   }
 }
--- src/main/java/io/camunda/demo/process_payments/ProcessPaymentsApplication.java
+++ src/main/java/io/camunda/demo/process_payments/ProcessPaymentsApplication.java
@@ -1,38 +1,16 @@
 package io.camunda.demo.process_payments;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.camunda.zeebe.client.ZeebeClient;
-
 @SpringBootApplication
-public class ProcessPaymentsApplication implements CommandLineRunner {
+public class ProcessPaymentsApplication {
 
   private static final Logger LOG = LoggerFactory.getLogger(ProcessPaymentsApplication.class);
 
-  @Autowired
-  private ZeebeClient zeebeClient;
-
   public static void main(String[] args) {
     SpringApplication.run(ProcessPaymentsApplication.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-    var bpmnProcessId = "process-payments";
-    var event = zeebeClient.newCreateInstanceCommand()
-        .bpmnProcessId(bpmnProcessId)
-        .latestVersion()
-        .variables(Map.of("total", 100000))
-        .send()
-        .join();
-
-    LOG.info("started a process instance: {}", event.getProcessInstanceKey());
   }
 }
